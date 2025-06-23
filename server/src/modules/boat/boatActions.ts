f1df2e2c0b044bd309144b6787e8b4b6ab3c645f
darasom69 <daha69@hotmail.fr>
2025-06-23 11:00:55 +0200

    test(step3): etape terminer

--- server/src/modules/boat/boatActions.ts
+++ server/src/modules/boat/boatActions.ts
@@ -16,7 +16,34 @@ const browse: RequestHandler = async (req, res, next) => {
 };
 
 const edit: RequestHandler = async (req, res, next) => {
-  // your code here
+  try {
+    //on recup l'ID du bateau, on le convertit en nombre
+    const id = Number(req.params.id);
+    //on recup les nouvelles coordonnees
+    const { coord_x, coord_y } = req.body;
+    // verifie que 3 champs sont renseignés, si l'un d'eux est vide, on renvoie une erreur 400
+    if (!id || coord_x == null || coord_y == null) {
+      res
+        .status(400)
+        .json({ error: "Manque l'ID du bateau ou les coordonnées" });
+      return;
+    }
+    //met à jour le bateau avec les nouvelles coordonnées, on utilise le repository pour faire la requete SQL
+    const affectedBoats = await boatRepository.update({
+      id,
+      coord_x,
+      coord_y,
+    });
+    // Si aun bateau n'a été affecté, on renvoie une erreur 404
+    if (affectedBoats === 0) {
+      res.status(404).json({ error: "Bateau non trouvé" });
+    }
+    // Si tout s'est bien passé, on renvoie un status 204 (rien à afficher)
+    res.sendStatus(204);
+  } catch (err) {
+    // Pass any errors to the error-handling middleware
+    next(err);
+  }
 };
 
 export default {
--- server/src/modules/boat/boatRepository.ts
+++ server/src/modules/boat/boatRepository.ts
@@ -21,8 +21,11 @@ class BoatRepository {
   }
 
   async update(boatToUpdate: Partial<Boat>) {
-    // your code here
-    return 0;
+    const [result] = await databaseClient.query<Result>(
+      "update boat set coord_x = ?, coord_y = ? where id = ?",
+      [boatToUpdate.coord_x, boatToUpdate.coord_y, boatToUpdate.id],
+    );
+    return result.affectedRows;
   }
 }
 
