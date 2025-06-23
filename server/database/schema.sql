2c7ba920d58dc0ea206ad82d980389a6ea4bfa9f
Alexandra PASTOR <pastoralex@free.fr>
2025-06-23 09:25:25 +0200

    step1

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -9,6 +9,7 @@ CREATE TABLE boat (
 CREATE TABLE tile (
     id INT AUTO_INCREMENT NOT NULL,
     type VARCHAR(255) NOT NULL,
+    has_treasure BOOLEAN NOT NULL DEFAULT FALSE,
     coord_x INT NOT NULL,
     coord_y INT NOT NULL,
     PRIMARY KEY (id)
