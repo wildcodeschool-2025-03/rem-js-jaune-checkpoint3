126c661754c5386efda5385fa2cd1f5551f46075
GUILLAUME ZIMBERLIN <zimberlinguillaume@gmail.com>
2025-06-23 11:13:12 +0200

    fixes

--- server/src/modules/boat/boatRepository.ts
+++ server/src/modules/boat/boatRepository.ts
@@ -1,5 +1,4 @@
 import databaseClient from "../../../database/client";
-
 import type { Result, Rows } from "../../../database/client";
 
 type Boat = {
@@ -11,22 +10,24 @@ type Boat = {
 
 class BoatRepository {
   async readAll(where: { name?: string } = {}) {
-    if (where == null) {
-      // Execute the SQL SELECT query to retrieve all boats from the "boat" table
+    if (where.name) {
       const [rows] = await databaseClient.query<Rows>(
-        "select b.id, b.coord_x, b.coord_y, b.name, t.type, t.has_treasure FROM boat AS b JOIN tile AS t ON t.coord_x = b.coord_x AND t.coord_y = b.coord_y order by coord_y, coord_x",
+        `SELECT b.id, b.coord_x, b.coord_y, b.name, t.type, t.has_treasure
+       FROM boat AS b
+       JOIN tile AS t ON t.coord_x = b.coord_x AND t.coord_y = b.coord_y
+       WHERE b.name = ?
+       ORDER BY b.coord_y, b.coord_x`,
+        [where.name],
       );
-
-      // Return the array of tiles
       return rows as Boat[];
     }
-    // Execute the SQL SELECT query to retrieve all boats from the "boat" table
-    const [rows] = await databaseClient.query<Rows>(
-      "select b.id, b.coord_x, b.coord_y, b.name, t.type, t.has_treasure FROM boat AS b JOIN tile AS t ON t.coord_x = b.coord_x AND t.coord_y = b.coord_y AND b.name = ? order by coord_y, coord_x ",
-      [where.name],
-    );
 
-    // Return the array of tiles
+    const [rows] = await databaseClient.query<Rows>(
+      `SELECT b.id, b.coord_x, b.coord_y, b.name, t.type, t.has_treasure
+     FROM boat AS b
+     JOIN tile AS t ON t.coord_x = b.coord_x AND t.coord_y = b.coord_y
+     ORDER BY b.coord_y, b.coord_x`,
+    );
     return rows as Boat[];
   }
 
@@ -35,7 +36,6 @@ class BoatRepository {
       "UPDATE boat SET coord_x = ?, coord_y = ? WHERE id = ?",
       [boatToUpdate.coord_x, boatToUpdate.coord_y, boatToUpdate.id],
     );
-
     return result.affectedRows;
   }
 }
