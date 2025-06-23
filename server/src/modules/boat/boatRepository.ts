e9b4a82839fc1a20aa23447da6fa08edc6e8fcda
Alexandra PASTOR <pastoralex@free.fr>
2025-06-23 10:55:32 +0200

    step5

--- server/src/modules/boat/boatRepository.ts
+++ server/src/modules/boat/boatRepository.ts
@@ -1,5 +1,6 @@
 import databaseClient from "../../../database/client";
 
+import type { RowDataPacket } from "mysql2";
 import type { Result, Rows } from "../../../database/client";
 
 type Boat = {
@@ -9,20 +10,32 @@ type Boat = {
   coord_y: number;
 };
 
-class BoatRepository {
-  async readAll(where = {}) {
-    // Execute the SQL SELECT query to retrieve all boats from the "boat" table
-    const [rows] = await databaseClient.query<Rows>(
-      "select * from boat order by coord_y, coord_x",
-    );
+type BoatWithTile = {
+  id: number;
+  name: string;
+  coord_x: number;
+  coord_y: number;
+  type: string;
+  has_treasure: boolean;
+};
 
-    // Return the array of tiles
-    return rows as Boat[];
+class BoatRepository {
+  async readAll(where = {}): Promise<BoatWithTile[]> {
+    // Execute the SQL SELECT query to retrieve all boats from the "boat" table
+    const [rows] = await databaseClient.query<RowDataPacket[]>(`
+      SELECT boat.id, boat.name, boat.coord_x, boat.coord_y, tile.type, tile.has_treasure
+      FROM boat
+      JOIN tile ON boat.coord_x = tile.coord_x AND boat.coord_y = tile.coord_y
+      ORDER BY boat.coord_y, boat.coord_x
+    `);
+
+    // Return the array of boats with their associated tiles
+    return rows as BoatWithTile[];
   }
 
   async update(boat: Boat) {
     const [result] = await databaseClient.query<Result>(
-      "update boat set name = ?, coord_x = ?, coord_y = ? where id = ?",
+      "UPDATE boat SET name = ?, coord_x = ?, coord_y = ? WHERE id = ?",
       [boat.name, boat.coord_x, boat.coord_y, boat.id],
     );
     return result.affectedRows;
