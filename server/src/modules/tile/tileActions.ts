96e95edc3ed3790ab982c47b42f211da9da88287
Alexandra PASTOR <pastoralex@free.fr>
2025-06-23 09:29:34 +0200

    step2

--- server/src/modules/tile/tileActions.ts
+++ server/src/modules/tile/tileActions.ts
@@ -1,7 +1,13 @@
 import type { RequestHandler } from "express";
+import tileRepository from "./tileRepository";
 
 const browse: RequestHandler = async (req, res, next) => {
-  // your code here
+  try {
+    const tiles = await tileRepository.readAll();
+    res.json(tiles);
+  } catch (error) {
+    next(error);
+  }
 };
 
 const validate: RequestHandler = async (req, res, next) => {
